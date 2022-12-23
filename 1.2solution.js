d2851a1fe4e981142b04a447375dbee4409c8f08
Deepal Kumar <18352652+Deepak656@users.noreply.github.com>
2022-12-24 02:45:27 +0530

    Update 1.2solution.js

--- 1.2solution.js
+++ 1.2solution.js
@@ -1,1 +1,24 @@
 
+// Using the elements and forms arrays of the Document object
+Suppose you had a piece of code as below
+
+<form action="blah.php"> 
+     <input type="button" name="someButton" /> 
+</form>
+
+// So you could access the button as:
+var something = document.forms[0].elements[0]; 
+
+
+// Using element names
+Suppose you had a piece of code as below
+
+<form name="blahForm" action="blah.php" /> 
+      <input type="button" name="blahButton" /> 
+</form> 
+
+//So you could access the button as:
+var something = document.blahForm.blahButton; 
+
+Source : https://qr.ae/prhvsL
+
