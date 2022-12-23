744018d653a04cf0dfa5fb7b70ec3958bafd80ab
Deepal Kumar <18352652+Deepak656@users.noreply.github.com>
2022-12-24 02:21:46 +0530

    Update 8.2_solution.js

--- 8.2_solution.js
+++ 8.2_solution.js
@@ -1,1 +1,20 @@
+// Given a number n find out if it is prime number or not
+// Use javascript to find out difference between next prime number after X and X
+
+PRIME NUMBER OR NOT
+
+if ( n == 1 ) {
+  console.log("Neither prime nor composite");
+}
+else if (n > 1) {
+  for (let i = 2; i < number; i++ ) {
+    if (number%i == 0) :
+      console.log("Not a prime number");
+      break;
+  }
+}
+else {
+  console.log("Not a prime number");
+};
+
 
