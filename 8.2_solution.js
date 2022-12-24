2a48719c9ce78b2b695b1c52752c36a79939c820
Deepal Kumar <18352652+Deepak656@users.noreply.github.com>
2022-12-24 15:59:45 +0530

    Update 8.2_solution.js

--- 8.2_solution.js
+++ 8.2_solution.js
@@ -3,19 +3,20 @@
 
 PRIME NUMBER OR NOT
 
+n = 17
 if ( n == 1 ) {
   console.log("Neither prime nor composite");
 }
 else if (n > 1) {
-  for (let i = 2; i < number; i++ ) {
-    if (number%i == 0) {
+  for (let i = 2; i < n; i++ ) {
+    if (n%i == 0) {
       console.log("Not a prime number");
       break;
     }
   }
+  console.log("Prime number");
 }
 else {
   console.log("Not a prime number");
-};
-
+}
 
